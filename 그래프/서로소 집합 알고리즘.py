28104e9ae8c8ff1b6fe5bc33434560c7d94cfc06
asebn1 <asebn1@khu.ac.kr>
2021-05-29 01:00:00 +0000

    서로소

---
+++ 그래프/서로소 집합 알고리즘.py
@@ -0,0 +1,47 @@
+"""
+parent      1) [0]노드생성   2) index별로초기화
+1. find_parent
+2. union_parent      1) find_parent 사용
+"""
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+# 두 원소가 속한 집합 합침
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수, 간선의 개수(union 연산)
+v, e = 6, 4 # 노드 1,2,3,4,5,6
+parent = [0] * (v+1) # 부모 테이블 초기화
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, v+1):
+    parent[i] = i
+
+#union 연산을 각각 수행
+# e번. 4번 
+union_parent(parent, 1, 4)
+union_parent(parent, 2, 3)
+union_parent(parent, 2, 4)
+union_parent(parent, 5, 6)
+
+# 출력
+print('각 원소가 속한 집합 : ', end='')
+for i in range(1, v+1):
+    print(find_parent(parent, i), end=' ')
+print()
+
+# 부모 테이블 내용 출력
+print('부모 테이블 : ', end='')
+for i in range(1, v+1):
+    print(parent[i], end=' ')
