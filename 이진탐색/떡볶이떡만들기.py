93a479b1969b7aed63fb7e0cc217870beefbd319
asebn1 <asebn1@khu.ac.kr>
2021-06-10 01:00:00 +0000

    이진탐색 1

---
+++ 이진탐색/떡볶이떡만들기.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+start = 0
+end = max(arr)
+
+# 이진 탐색
+result = 0
+while start<=end:
+    total = 0
+    mid = (start+end) // 2  # 9
+    for x in arr:
+        if x > mid:
+            total += x-mid
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid+1
+print(result)
