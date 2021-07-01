ba3150e4ca38e12a7b70577a3aa9ad9bf0abc72f
asebn1 <asebn1@khu.ac.kr>
2021-07-01 01:00:00 +0000

    버블

---
+++ bubble, heap, insert, select/Bubble.py
@@ -0,0 +1,12 @@
+s=[3,2,5,7,1,9,4,6,8]
+n = len(s)
+
+for i in range(n,-1,-1):
+    for j in range(1,i):
+        if s[j-1] > s[j]:
+            temp = s[j]
+            s[j] = s[j-1]
+            s[j-
+              1] = temp
+           
+print(s)
