ca4070ab9ca1a52417ed3ee95ae1598eb4620e6b
asebn1 <asebn1@khu.ac.kr>
2021-07-07 01:00:00 +0000

    다익스트라1

---
+++ dijkstra, huffman/dijkstra.py
@@ -0,0 +1,29 @@
+inf=1000
+w=[[0,7,4,6,1],[inf,0,inf,inf,inf],
+   [inf,2,0,5,inf], [inf,3,inf,0,inf], [inf,inf,inf,1,0]]
+n=5
+f=set()
+touch=n*[0]
+length=n*[0]
+
+for i in range(1, n):
+    length[i] = w[0][i]
+
+
+for k in range(1, n):
+    vnear = -1
+    min = inf
+    for i in range(1, 5):
+        if( 0<= length[i] < min):
+            min = length[i]
+            vnear = i
+    e = (touch[vnear], vnear)
+    f.add(e)
+    for i in range(1, n):
+        if(length[vnear] + w[vnear][i] < length[i]):
+            length[i] = length[vnear] + w[vnear][i]
+            touch[i] = vnear
+    length[vnear] = -1
+
+print(f)
+            
