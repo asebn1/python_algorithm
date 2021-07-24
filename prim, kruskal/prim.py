17121781c9ce79f11f6e6c0392255b61ce42c8b1
asebn1 <asebn1@khu.ac.kr>
2021-07-24 01:00:00 +0000

    prim

---
+++ prim, kruskal/prim.py
@@ -0,0 +1,35 @@
+import utility
+
+inf = 1000
+w=[[0,1,3,inf,inf],
+   [1,0,3,6,inf],
+   [3,3,0,4,2],
+   [inf,6,4,0,5],
+   [inf,inf,2,5,0]]
+
+F=set()
+utility.printMatrix(w)
+n=len(w)
+nearest=n*[0]
+distance=n*[0]
+for i in range(1,n):
+    nearest[i]=0
+    distance[i]=w[0][i]
+
+for k in range(1,n):
+    min = inf
+    vnear = -1
+    for i in range(1,n):
+        if(0<=distance[i]<min):
+            min = distance[i]
+            vnear = i
+    e = (vnear,nearest[vnear])
+    F.add(e)
+    distance[vnear] = -1
+    for i in range(1,n):
+        if(w[i][vnear] < distance[i]):
+            distance[i] = w[i][vnear]
+            nearest[i] = vnear
+
+print()
+print(F)
